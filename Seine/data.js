21a618edd50ee59d71fa2ee5398b7eef9c56df2e
YueFengYuan0904 <hankwu0904@gmail.com>
2024-11-08 12:02:43 +0800

    n94s84ck6

---
+++ Seine/data.js
@@ -0,0 +1,246 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "起點",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": 3.139049636613012,
+        "pitch": 0.05006041410094042,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1222814331133826,
+          "pitch": 0.4814023417838609,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.2001842260632545,
+          "pitch": 0.10994057865621443,
+          "title": "塞納河",
+          "text": "在優美的河上開啟旅程吧!!!"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "巴黎聖母院",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": -1.4015027018400907,
+        "pitch": -0.13340226138622846,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7934228102519292,
+          "pitch": 0.23610238210897094,
+          "rotation": 23.561944901923464,
+          "target": "0-"
+        },
+        {
+          "yaw": -0.8371953031050481,
+          "pitch": 0.12421493674989748,
+          "rotation": 0.7853981633974483,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.956006703232756,
+          "pitch": -0.615267812294757,
+          "title": "巴黎聖母院",
+          "text": "在2019年4月15日，發生了一場大火。目前仍在整修"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "羅浮宮",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.2950423305184025,
+        "pitch": -0.07759364185645623,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4923789626556925,
+          "pitch": 0.18957723698802198,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": 0.5191105970591483,
+          "pitch": 0.3961399566116839,
+          "rotation": 6.283185307179586,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.4908855579915254,
+          "pitch": -0.41447250093062316,
+          "title": "羅浮宮",
+          "text": "主要組成部分的總面積則超過60,600平方公尺（652,000平方英尺），館內永久收藏則包括雕塑、繪畫、美術工藝及古代東方、古代埃及和古希臘羅馬等7個分類，主要收藏1860年以前的藝術作品與考古文物。"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "蒙娜麗莎的微笑",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.32548349481156613,
+        "pitch": -0.06287872451166265,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.23525426900006075,
+          "pitch": 0.2881672620753353,
+          "rotation": 11.780972450961727,
+          "target": "4-"
+        },
+        {
+          "yaw": 2.3427601252662207,
+          "pitch": 0.04422931939537378,
+          "rotation": 0.7853981633974483,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.15861056978847898,
+          "pitch": -0.347547793511751,
+          "title": "蒙娜麗莎的微笑",
+          "text": "是一幅文藝復興時期畫家李奧納多·達文西所繪的肖像畫與其代表作。被認為是義大利文藝復興時期的傑作，也被視為世上最有名、訪問量最大且最常被寫、唱、模仿的藝術作品。"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "羅浮宮金字塔",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "yaw": 3.1394469411632455,
+        "pitch": -0.0566372944259772,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.2987682466274757,
+          "pitch": 0.25457870831443863,
+          "rotation": 0.7853981633974483,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.87158271458442,
+          "pitch": -0.49091204851275805,
+          "title": "羅浮宮金字塔",
+          "text": "是一個用玻璃和金屬建造的巨大金字塔，周圍環繞著三個較小的金字塔。大金字塔作為羅浮宮博物館的主入口。"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
